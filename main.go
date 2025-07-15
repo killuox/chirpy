e2b311b730a933f33eb9e1c3024a23c229db60cd
killuox <aarsenault.dev@gmail.com>
2025-07-15 09:10:44 -0400

    add metrics middleware + handlers

--- main.go
+++ main.go
@@ -3,23 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 const port = "8080"
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(http.Dir("./assets"))))
 
 	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
 	fmt.Printf("Server starting on port %s...\n", port)
+
 	// Start the server
 	err := server.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
@@ -29,6 +39,7 @@ func main() {
 	}
 }
 
+// Handlers
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
@@ -37,4 +48,37 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("Error writing response:", err)
 	}
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	currentHits := cfg.fileserverHits.Load()
+	responseBody := fmt.Appendf(nil, "Hits: %d", currentHits)
+	_, err := w.Write(responseBody)
+	if err != nil {
+		fmt.Println("Error writing response:", err)
+	}
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	cfg.fileserverHits.Store(0)
+	currentHits := cfg.fileserverHits.Load()
+	responseBody := fmt.Appendf(nil, "Hits: %d", currentHits)
+	_, err := w.Write(responseBody)
+	if err != nil {
+		fmt.Println("Error writing response:", err)
+	}
+}
+
+// Middlewares
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
