ebfc9f276c673353ee4507e42239045ef0b86c39
killuox <aarsenault.dev@gmail.com>
2025-07-17 20:31:41 -0400

    Add json part

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -16,12 +19,16 @@ func main() {
 	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 
+	// APP
 	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", http.FileServer(http.Dir("./assets"))))
-
-	mux.HandleFunc("/healthz", healthHandler)
-	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("/reset", apiCfg.resetHandler)
+	// ADMIN
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	// GENERAL
+	mux.HandleFunc("GET /healthz", healthHandler)
+	// API
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.validateChirpHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -51,11 +58,17 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
 	currentHits := cfg.fileserverHits.Load()
-	responseBody := fmt.Appendf(nil, "Hits: %d", currentHits)
+	responseBody := fmt.Appendf(nil, fmt.Sprintf(`
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`, currentHits))
 	_, err := w.Write(responseBody)
 	if err != nil {
 		fmt.Println("Error writing response:", err)
@@ -75,10 +88,102 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type parameters struct {
+	Body string `json:"body"`
+}
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+type validResponse struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	// Validate body length (140 chars)
+	bodyLength := len(params.Body)
+	fmt.Printf("the body length is: %v\n", bodyLength)
+	if bodyLength > 140 {
+		respondWithError(w, 500, "Chirp is too long")
+		return
+	}
+
+	// hide profane words
+	words := strings.Fields(params.Body)
+
+	sanitizedWords := make([]string, len(words))
+	for i, word := range words {
+		lowerWord := strings.ToLower(word)
+		if containsWord(profaneWords, lowerWord) {
+			sanitizedWords[i] = "****"
+		} else {
+			sanitizedWords[i] = word
+		}
+	}
+
+	// Add back spaces between words
+	paragraph := strings.Join(sanitizedWords, " ")
+
+	// Prepare repsonse
+	respBody := validResponse{
+		CleanedBody: paragraph,
+	}
+	respondWithJSON(w, 200, respBody)
+
+}
+
 // Middlewares
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
+}
+
+// Utilities
+
+func containsWord(slice []string, targetWord string) bool {
+	for _, word := range slice {
+		if word == targetWord {
+			return true
+		}
+	}
+	return false
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+	respBody := errorResponse{
+		Error: msg,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Write(dat)
+}
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.WriteHeader(code)
+	respBody := payload
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Write(dat)
+	w.WriteHeader(200)
 }
